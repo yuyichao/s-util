4324b2dd3fb47741a9f27631f27c9e62a0dfb53d
Yichao Yu <yyc1992@gmail.com>
2011-04-11 17:52:20 -0400

    Makefile is useless (can be finished by install) in this project s_init.sh is ok now

--- s_init.sh
+++ s_init.sh
@@ -1,9 +1,11 @@
 S_U_VERSION=0.0.1
-for ((i = 1;i < $#;i++)) ;do
+for ((i = 1;i <= $#;i++)) ;do
     case ${!i} in
 	-h|--help)
 	    if type s_shelp &> /dev/null ;then
 		s_shelp
+	    else
+		echo "HELP_INFO"
 	    fi
 	    exit
 	    ;;
