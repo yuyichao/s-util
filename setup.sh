26097430566d17f70c8f3869592ac470d15e435f
Yichao Yu <yyc1992@gmail.com>
2011-04-11 20:02:37 -0400

    install may work now

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
-S_UTIL_INSTALL_DIR=@@install_dir
+export S_UTIL_INSTALL_DIR=@@install_dir
 if [[ ":${PATH}:" =~ ":${S_UTIL_INSTALL_DIR}:" ]] ;then
-    PATH=${PATH}
+    export PATH=${PATH}
 else
-    PATH="${PATH}:${S_UTIL_INSTALL_DIR}"
+    export PATH="${PATH}:${S_UTIL_INSTALL_DIR}"
 fi
-export PATH
