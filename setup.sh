7c70a7af9c107f3e343b6df32d542f305e433209
Yichao Yu <yyc1992@gmail.com>
2011-04-29 10:18:27 -0400

    fix completiong !!!!!!!!!!!

--- setup.sh
+++ setup.sh
@@ -5,14 +5,32 @@ else
     export PATH="${PATH}:${S_UTIL_INSTALL_DIR}"
 fi
 
+_s_in_array()
+{
+    local i
+    for ((i = 2;i < $#;i++)) ;do
+	if [[ "$1" == "${!i}" ]] ;then
+	    return 0
+	fi
+    done
+    return 1
+}
+
 _s_util_general_args()
 {
-    local general_args='-h -v --version --help'
+    local general_args=(-h -v --version --help) i j n
     for ((i = 1;i < ${#COMP_WORDS[@]};i++)) ;do
 	if ((i == COMP_CWORD)) ;then
 	    continue
 	fi
-	if [[ " ${general} " =~ " ${COMP_WORDS[i]} " ]] ;then
+	if _s_in_array "${COMP_WORDS[i]}" "${general_args[@]}" ;then
+	    n=${#general_args[@]}
+	    for ((j = 0;j < n;j++)) ;do
+		if [[ "${COMP_WORDS[i]}" == "${general_args[j]}" ]] ;then
+		    unset general_args[j]
+		fi
+	    done
+	    general_args=("${general_args[@]}")
 	    COMP_WORDS=("${COMP_WORDS[@]:0:i}" "${COMP_WORDS[@]:i + 1}")
 	    if ((i < COMP_CWORD)) ;then
 		let 'COMP_CWORD--'
@@ -20,7 +38,7 @@ _s_util_general_args()
 	    let 'i--'
 	fi
     done
-    possible=(${general_args})
+    possible=("${general_args[@]}")
 }
 
 __s_util_g_comp()
@@ -29,13 +47,13 @@ __s_util_g_comp()
     command="${COMP_WORDS[0]}"
     cur="${COMP_WORDS[COMP_CWORD]}"
     _s_util_general_args
-    type "_${command}" && "_${command}"
-    COMPREPLY=($(compgen -W "${possible[@]}" -- ${cur}))
+    type "_${command}" &>/dev/null && "_${command}"
+    COMPREPLY=($(compgen -W "${possible[*]}" -- ${cur}))
 }
 
 _clpbd()
 {
-    local opts fopt
+    local opts fopt i j n
     if [ "$COMP_CWORD" == 1 ] ;then
 	opts="-c -d -p"
 	possible=("${possible[@]}" $(compgen -W "${opts}" -- ${cur}))
@@ -45,10 +63,34 @@ _clpbd()
 	case "${fopt}" in
 	    -c)
 		possible=("${possible[@]}" $(compgen -f ${cur}))
+		for ((i = 2;i < ${#COMP_WORDS[@]};i++)) ;do
+		    if ((i == COMP_CWORD)) ;then
+			continue
+		    fi
+		    n=${#possible[@]}
+		    for ((j = 0;j < n;j++)) ;do
+			if [[ "${COMP_WORDS[i]}" == "${possible[j]}" ]] ;then
+			    unset possible[j]
+			fi
+		    done
+		    possible=("${possible[@]}")
+		done
 		return 0
 		;;
 	    -p|-d)
 		possible=("${possible[@]}" $(cd /tmp/_s_clipboard 2> /dev/null && compgen -f ${cur}))
+		for ((i = 2;i < ${#COMP_WORDS[@]};i++)) ;do
+		    if ((i == COMP_CWORD)) ;then
+			continue
+		    fi
+		    n=${#possible[@]}
+		    for ((j = 0;j < n;j++)) ;do
+			if [[ "${COMP_WORDS[i]}" == "${possible[j]}" ]] ;then
+			    unset possible[j]
+			fi
+		    done
+		    possible=("${possible[@]}")
+		done
 		return 0
 		;;
 	    *)
@@ -58,9 +100,14 @@ _clpbd()
     fi
 }
 
-local complete_list
-complete_list=(addpkla cback cempty clpbd import-cert recget spath spid)
+reg_complete()
+{
+    local complete_list
+    complete_list=(addpkla cback cempty clpbd import-cert recget spath spid)
 
-for command in "${complete_list[@]}" ;do
-    complete -F __s_util_g_comp "${command}"
-done
+    for command in "${complete_list[@]}" ;do
+	complete -F __s_util_g_comp "${command}"
+    done
+}
+
+reg_complete
