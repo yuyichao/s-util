10837539bd6a24988b0b8b5510f0294def68c2b9
Yichao Yu <yyc1992@gmail.com>
2011-04-29 18:47:39 -0400

    6

--- s_init.sh
+++ s_init.sh
@@ -1,18 +1,31 @@
-S_U_VERSION=0.1.0
-for ((i = 1;i <= $#;)) ;do
-    case ${!i} in
+S_U_VERSION=0.2.0
+darg()
+{
+    if [[ $# == 0 ]] ;then
+	n=0
+    else
+	n=$1
+    fi
+    let 'n > 0' || return 1
+    args=("${args[@]:0:n}" "${args[@]:n + 1}")
+}
+
+args=("$@")
+set --
+for ((i = 0;i < ${#args[@]};)) ;do
+    case "${args[i]}" in
 	-h|--help)
 	    if type s_shelp &> /dev/null ;then
 		s_shelp
 	    else
 		echo "$HELP_INFO"
 	    fi
-	    shift $i
+	    darg $i
 	    exit
 	    ;;
 	-v|--version)
 	    echo ${S_U_VERSION}
-	    shift $i
+	    darg $i
 	    exit
 	    ;;
 	*)
