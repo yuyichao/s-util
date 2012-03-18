cc876629f604ab81f3ebcc3a09ab16b8148cd0fd
Yichao Yu <yyc1992@gmail.com>
2012-03-18 08:01:35 -0400

    bash-completion

--- profile.sh
+++ profile.sh
@@ -7,3 +7,18 @@ d() {
     done
     cd "$path"
 }
+
+_dirlst2array() {
+    local IFS=:
+    eval "$1"='($2)'
+}
+
+__s_util_init_alias() {
+    local line alias_f
+    alias_f=()
+    ! [[ -z $XDG_CONFIG_DIRS ]] && {
+        _dirlst2array xdg_dirs "$XDG_CONFIG_DIRS"
+    }
+    while read line; do
+    done
+}
